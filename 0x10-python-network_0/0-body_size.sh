eebf224f037335caabdc8ec55ae7715cbe4f16d6
AserZakaria <asserzakaria@gmail.com>
2024-03-28 06:39:20 -0700

    A

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if the correct number of arguments are provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-# Send a request to the URL and save the response body to a temporary file
-response=$(curl -sI "$URL")
-body_size=$(echo "$response" | awk '/Content-Length/ {print $2}')
-
-# Display Size of the body in bytes
-echo "$body_size"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
